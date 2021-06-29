360cc52097a9151d719bfcb7a04020d7cd03f37f
Marcelo Marcon <marcelomarcon@outlook.com>
2021-06-29 10:42:13 -0400

    criado campo de sequencia para codigo

--- contract/models/account_contrato_consorcio.py
+++ contract/models/account_contrato_consorcio.py
@@ -6,11 +6,25 @@ class ContratoConsorcio(models.Model):
     _name = 'contract.contrato_consorcio'
     _description = 'Contrato Consorcio'
 
-    name = fields.Char(string="Código")
+    name = fields.Char(
+        string="Código", 
+        default="New", 
+        copy=False, 
+        index=True, 
+        readonly=True)
     cd_descricao = fields.Text()
     cd_ativo = fields.Boolean(default=False)
 
     contratos = fields.One2many('contract.contrato_consorcio_linha', 'contrato_id', string='Contratos')
+
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', 'New') == 'New':
+            vals['name'] = self.env['ir.sequence'].next_by_code(
+                'contract.contrato_consorcio')
+
+        return super().create(vals)
 
 class ContratoConsorcioLinha(models.Model):
     _name = 'contract.contrato_consorcio_linha'
@@ -25,7 +39,7 @@ class ContratoConsorcioLinha(models.Model):
 
     contrato_id = fields.Many2one('contract.contrato_consorcio', string="Contrato")
 
-    @api.constrains('cd_ativo')
+    @api.constrains('cd_participacao')
     def verificar_porcentagem(self):
-        if self.cd_ativo < 0 or self.cd_ativo > 100:
+        if self.cd_participacao < 0 or self.cd_participacao > 100:
             raise ValidationError("Campo participação deve ser maior que 0 e menor que 100!")
